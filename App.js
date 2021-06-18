3f061f6a0f75abc70e90367aa26fb649bc049ff8
Mårten Pettersson <mtnptrsn@gmail.com>
2021-06-18 08:37:31 +0200

    Add bug

--- App.js
+++ App.js
@@ -1,16 +1,91 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import WebView from "react-native-webview";
 
 export default function App() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        // justifyContent: "center",
+        // alignItems: "center",
       }}
     >
-      <Text>Universal React with Expo</Text>
+      <View style={{ flex: 1, backgroundColor: "white" }}>
+        <ScrollView contentContainerStyle={{ width: "100%" }}>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <WebView
+            style={{ height: 500, width: "100%" }}
+            source={{
+              html: `
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <p>------</p>
+            <p>This is a p tag inside the web view</p>
+            <p>This is a p tag inside the web view</p>
+            <p>This is a p tag inside the web view</p>
+            <p>This is a p tag inside the web view</p>
+            <p>This is a p tag inside the web view</p>
+            <p>------</p>
+          `,
+            }}
+            originWhitelist={["*"]}
+          />
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+          <Text>This is a native text component</Text>
+        </ScrollView>
+      </View>
     </View>
   );
 }
